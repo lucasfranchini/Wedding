e57309f40eed80f6c504e1cd68d003e82ed56a6e
lucasfranchini <lucasfranchini.a@gmail.com>
2023-05-04 20:56:39 -0300

    feat: startPage Content

--- src/app/(startPage)/layout.tsx
+++ src/app/(startPage)/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
+import Header from "../header";
 import "./globals.css";
-import Header from "./header";
 
 const inter = Inter({ subsets: ["latin"] });
 
---
+++ src/app/(startPage)/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  const style = {
+    backgroundImage: "url(/fabioEMartha.jpeg)",
+  };
+  return (
+    <main>
+      <div className="h-screen bg-cover bg-opacity-60" style={style}></div>
+    </main>
+  );
+}
