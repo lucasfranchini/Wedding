ad9822a2a2d3c61e225090cc8a504e1230b1f971
lucasfranchini <lucasfranchini.a@gmail.com>
2023-05-05 00:29:21 -0300

    feat: names Layout

--- src/app/(startPage)/page.tsx
+++ src/app/(startPage)/page.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
+
 export default function Home() {
   const style = {
     backgroundImage: "url(/fabioEMartha.jpeg)",
   };
   return (
     <main>
-      <div className="h-screen bg-cover bg-opacity-60" style={style}></div>
+      <div
+        className="flex justify-end items-center h-96 sm:h-screen bg-cover bg-opacity-60 bg-no-repeat"
+        style={style}
+      >
+        <div className="hidden xl:block xl:mr-10">
+          <Image src="/FMComplete.png" alt="F&M" width={500} height={40} />
+        </div>
+      </div>
+      <div className="flex justify-center xl:hidden ">
+        <Image src="/FMComplete.png" alt="F&M" width={500} height={40} />
+      </div>
     </main>
   );
 }
--- src/app/header/index.tsx
+++ src/app/header/index.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import "./style.css";
 
 const Header: React.FC = () => {
   return (
-    <div className="flex justify-around items-center w-full p-5 fixed top-0 left-0 bg-gray-900 bg-opacity-5">
+    <div className="flex justify-around items-center w-full p-5 fixed top-0 left-0 bg-gray-900 bg-opacity-5 text-xl">
       <Image src="/FMClear.png" alt="F&M" width={100} height={40} />
-      <div className="flex items-center gap-5 ">
+      <div className="flex items-center gap-5 Container">
         <Link href="/">Inicio</Link>
         <Link href="/gifts">Presentes</Link>
         <Link href="/confirm">Confirmar Presença</Link>
