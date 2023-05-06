d95e16e64356ea115e038ac83dede1d0c161b204
lucasfranchini <lucasfranchini.a@gmail.com>
2023-05-06 00:30:46 -0300

    feat: start confirmation page

---
+++ src/app/confirm/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Confirmar Presença",
+  description: "Confirmar presença no Casamento de Fabio e Martha",
+};
+
+export default function GiftsLayou({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
+}
---
+++ src/app/confirm/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const ConfirmPage: NextPage = () => {
+  return (
+    <div className="flex flex-col items-center h-screen w-full bg-[#f7f7f7] font-mont pb-20"></div>
+  );
+};
+
+export default ConfirmPage;
