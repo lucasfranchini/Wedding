750dc661cf88869159b649cf091ff5ef7691198a
lucasfranchini <lucasfranchini.a@gmail.com>
2023-05-06 00:25:12 -0300

    chore: add gifts layout

---
+++ src/app/gifts/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Lista de Presentes",
+  description: "Lista de Presentes do Casamento de Fabio e Martha",
+};
+
+export default function GiftsLayou({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}s</div>;
+}
