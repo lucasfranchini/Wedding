623b6a3a7dcd92fe54c434158d7b1c42d0c2fe05
lucasfranchini <lucasfranchini.a@gmail.com>
2023-05-06 00:42:48 -0300

    feat: title and description

--- src/app/confirm/page.tsx
+++ src/app/confirm/page.tsx
@@ -2,7 +2,12 @@ import { NextPage } from "next";
 
 const ConfirmPage: NextPage = () => {
   return (
-    <div className="flex flex-col items-center h-screen w-full bg-[#f7f7f7] font-mont pb-20"></div>
+    <div className="flex flex-col items-center h-screen w-full bg-[#f7f7f7] font-mont pb-20">
+      <h1 className="mt-36 mb-14 text-black text-2xl">Confirmar Presença</h1>
+      <span className="text-lg text-gray-700">
+        Confirme sua presença nessa data tão especial para nós.
+      </span>
+    </div>
   );
 };
 
